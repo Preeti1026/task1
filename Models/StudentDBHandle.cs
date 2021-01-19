4d817733606209d490a1b0e1e84df449637bd755
Preeti1026 <77668675+Preeti1026@users.noreply.github.com>
2021-01-19 02:55:12 -0800

    Add files via upload

---
+++ Models/StudentDBHandle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CRUDinMVC.Models
+{
+    public class StudentDBHandle
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["studentconn"].ToString();
+            con = new SqlConnection(constring);
+        }
+        // **************** ADD NEW STUDENT *********************
+        public bool AddStudent(StudentModel smodel)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("AddNewStudent", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", smodel.Name);
+            cmd.Parameters.AddWithValue("@City", smodel.City);
+            cmd.Parameters.AddWithValue("@Address", smodel.Address);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+
+
+}
